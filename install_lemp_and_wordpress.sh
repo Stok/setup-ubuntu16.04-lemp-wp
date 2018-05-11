563288fcc621fc89903ab49a1d5c5e3979e212ff
Stok <sean@stokedprojects.org>
2018-05-12 00:42:48 +0200

    return home on cd

--- install_lemp_and_wordpress.sh
+++ install_lemp_and_wordpress.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+# All scripts should be cloned into home (shouldn't really matter, but untested when used from elsewhere)
+cd ~
 git clone https://github.com/Stok/setup-ubuntu16.04-lemp.git
 cd setup-ubuntu16.04-lemp
 source ./install_lemp.sh
 
 # Return to the home directory for second installation
 cd ~
-
 git clone https://github.com/Stok/setup-ubuntu16.04-wordpress.git
 cd setup-ubuntu16.04-wordpress
 source ./install_wordpress.sh
